303e652d3ef2674a3ac4020e612aa8283132312f
mazarini <mazarini@protonmail.com>
2023-12-04 20:01:14 +0100

    Clean composer and actions

--- config/bundles.php
+++ config/bundles.php
@@ -5,5 +5,4 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
